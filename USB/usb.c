8a7085d8b17e0f27e3f70544a8df974dde443a9d
Caique Garbin <caique.exe@gmail.com>
2019-06-13 13:58:53 -0300

    Modificacoes feitas para interagir com o motor

--- USB/usb.c
+++ USB/usb.c
@@ -10,9 +10,34 @@
 #include <linux/usbdevice_fs.h>
 
 
-#define JS_EVENT_BUTTON	0x01 /*buttonpressed/released*/
-#define JS_EVENT_AXIS	0x02 /*joystickmoved*/
-#define JS_EVENT_INIT	0x80 /*initialstateofdevice*/
+#define JS_EVENT_BUTTON	0x01	/*buttonpressed/released*/
+#define JS_EVENT_AXIS	0x02	/*joystickmoved*/
+#define JS_EVENT_INIT	0x80	/*initialstateofdevice*/
+
+#define Axis_Range	32757
+#define PWM 		256	//PWM max
+
+//Para ganhar processamento, vou criar a variavel global e fazer apenas a operação na função
+long long conversion = Axis_Range/PWM;
+double vmax = 0;
+int REng;		//Right Engine
+int LEng;		//Left Engine
+
+
+//About engine control
+void engine (int number, int value){
+	if (number == 1){
+		vmax = value;
+	}else{
+		if(value > 0){ 					//car is turning to right side	
+			LEng = vmax; 				//Left wheel full speed
+			REng = vmax - vmax*(value/Axis_Range); 	//Right wheel slower
+		}else{
+			REng = vmax;
+			LEng = vmax - vmax*(value/Axis_Range);
+		}
+	}
+}
 
 
 struct js_event{
@@ -23,30 +48,34 @@ unsigned char 	number;/*axis/buttonnumber*/
 };
 
 
+
 int main(){
 
 	int fd = open("/dev/input/js0",O_RDONLY);
 
 	if (fd<0){
-		printf("cannotopendev\n");
-
+		//Canot open dev blink Red LED
+		
 	}else{
-		printf("opened success...:)\n");
+		//Sucess bright Green LED
 	}
 
 
 	struct js_event e;
 
+		//e.number number of button/axis
+		//e.value value read
+		
+
 	while(1)//eventloop
 		{
 		read(fd, &e, sizeof(e));
 
-		
 		if(e.type == JS_EVENT_BUTTON || e.type == JS_EVENT_AXIS){
 			if(e.type == JS_EVENT_BUTTON){
-				printf("button#%d value:%d\n",(int) e.number, e.value);
+				//Put a Switch function to do things when pushing buttons
 			}else{
-				printf("axis#%d value:%d\n",(int) e.number, e.value);
+				engine(e.number, e.value);	//Call Engine
 			}
 		}else{
 			printf("InitEvents\n");
