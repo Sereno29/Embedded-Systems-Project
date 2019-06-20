46e2f7bc46bc7806198550f64b00783e2d19cded
Leonardo Felipe L. S. dos Santos <leonardo.felipe.santos@usp.br>
2019-06-19 21:18:25 -0300

    Update motor.h

--- Final Code/motor.h
+++ Final Code/motor.h
@@ -5,13 +5,13 @@
 
     // FUNCTIONS HEADERS
     
-    void accelerate_motor_right(Direction direction, int duty_c);       // function to accelerate the right motor, direction is the rotation (clockwise or counterclockwise) and duty_c is the duty cycle percentage
+    void accelerate_motor_right(Direction direction, int duty_c);       // function to accelerate the right motor, direction is the rotation (clockwise cw or counterclockwise ccw) and duty_c is the duty cycle percentage
     void accelerate_motor_left(Direction direction, int duty_c);        // function to accelerate the left motor, direction is the rotation (clockwise or counterclockwise) and duty_c is the duty cycle percentage
     void go_forward(int duty_c);                                        // function to move forward accelerating both motors using the duty_c value
     void turn_right(int level, int duty_c);                             // function to turn right, proportionally to the level value
     void turn_left(int level, int duty_c);                              // function to turn left, proportionally to the level value
     void go_backwards(int duty_c);                                      // function to move backward accelerating both motors using the duty_c value
-    void rotate(Direction direction, int duty_c);                       // function to rotate the car around your own axis, in ohter words a curve with one motor off
+    void rotate(Direction direction, int duty_c);                       // function to rotate the car around your own axis, with one motor in cw and the other ccw
     void stop(void);                                                    // function to stop the car, leave the motors free
     void braking(void);                                                 // function to brake the car actively
     void disable_motors(void);                                          // function to disable the motors, configuring the pins back to gpio
